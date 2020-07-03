1a75cfccec133e648b6057416ab0a4ff206755af
anyataa <anyatmr@gmail.com>
2020-07-03 17:45:28 +0700

    1. Design homepage 2. Lessons preview 3. idea for drag component
    TODO:
    Minigames for drag games for process

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './main/home/home.component';
+import { LessonsComponent } from './main/lessons/lessons.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component:HomeComponent},
+  {path:'', redirectTo:'/home', pathMatch:'full'},
+  {path:'lessons', component:LessonsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './main/home/home.component';
+import { LessonsComponent } from './main/lessons/lessons.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LessonsComponent
   ],
   imports: [
     BrowserModule,
